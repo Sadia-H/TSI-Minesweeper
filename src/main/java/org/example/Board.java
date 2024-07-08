2d436bd05375f92ee1b2249a5903b67a9a2f36a2
Sadia-H <sadiahussain118@gmail.com>
2024-07-08 10:45:55 +0100

    Count adjacent mines method

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -5,93 +5,90 @@ import java.util.Random;
 
 public class Board {
 
-    private int[][] playerBoard;
-    private boolean[][] revealedBoard;
     private int boardSize;
     private int numMines;
+    private Tile[][] playerBoard;
 
     public Board(int boardSize, int numMines) {
         this.boardSize = boardSize;
         this.numMines = numMines;
-        this.playerBoard = new int[boardSize][boardSize];
-        this.revealedBoard = new boolean[boardSize][boardSize];
+        this.playerBoard = new Tile [boardSize][boardSize];
+        for (int i = 0; i<boardSize; i++) {
+            for (int j = 0; j<boardSize; j++) {
+                playerBoard[i][j] = new Tile();
+            }
+        }
         createBoard();
     }
 
 
     public void createBoard() {
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                playerBoard[i][j] = 0;
-                revealedBoard[i][j] = false;
+
+        //Random mine placement
+        Random rand = new Random();
+        int currentMines = 0;
+        while (currentMines < numMines) {
+            int row = rand.nextInt(boardSize);
+            int col = rand.nextInt(boardSize);
+
+            if (!playerBoard[row][col].isMine()) {
+                playerBoard[row][col].setMine(true);
+                currentMines++;
             }
         }
 
-        //placeRandomMines();
-        displayBoard();
+        for (int i = 0; i<boardSize; i++) {
+            for (int j = 0; j<boardSize;j++) {
+                if (!playerBoard[i][j].isMine()) {
+                    playerBoard[i][j].setAdjacentMines(countAdjacebtMines(i, j));
+
+                }
+            }
+        }
+
 
     }
 
-    public void displayBoard() {
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                if (revealedBoard[i][j]) {
-                    int tileValue = playerBoard[i][j];
-                    if (tileValue == -1) {
-                        System.out.print('*');
-                    } else if (tileValue == 0) {
-                        System.out.print('0');
+    private int countAdjacebtMines(int row, int col) {
+        int mineCount = 0;
+        for (int i = 0; i< boardSize; i++) {
+            for (int j = 0; j<boardSize; j++) {
+                if (i==0 && j==0) continue;
+                int adjRow = row + 1;
+                int adjCol = col + 1;
+                if (adjRow >= 0 && adjRow < boardSize
+                    && adjCol >= 0 && adjCol <boardSize) {
+                    if (playerBoard[adjCol][adjCol].isMine()) {
+                        mineCount ++;
                     }
-
-                } else {
-                    System.out.println('-');
                 }
-
-
-                System.out.println();
             }
         }
     }
 
 //    public void displayBoard() {
-//        for (int i = 0; i<boardSize; i++) {
-//            for (int j = 0; j<boardSize; j++) {
-//                if (playerBoard[i][j] == 0) {
-//                    System.out.print('-');
-//                }
-//                if (playerBoard[i][j] == -1) {
-//                    System.out.print('*');
+//        for (int i = 0; i < boardSize; i++) {
+//            for (int j = 0; j < boardSize; j++) {
+//                if (revealedBoard[i][j]) {
+//                    int tileValue = playerBoard[i][j];
+//                    if (tileValue == -1) {
+//                        System.out.print('*');
+//                    } else if (tileValue == 0) {
+//                        System.out.print('0');
+//                    }
+//
+//                } else {
+//                    System.out.println('-');
 //                }
 //
+//
+//                System.out.println();
 //            }
-//            System.out.println();
 //        }
 //    }
 
 
-//        public void revealBoard(int userSelectedRow,int userSelectedCol) {
-//
-//        }
 
-    public void revealBoard(int userSelectedRow, int userSelectedCol) {
-        revealedBoard[userSelectedRow][userSelectedCol] = true;
-        displayBoard();
-    }
-
-
-    public void placeRandomMines(int numMines) {
-        Random rand = new Random();
-
-        int currentMines = 0;
-        while (currentMines < numMines) {
-            int row = rand.nextInt(boardSize);
-            int col = rand.nextInt(boardSize);
-            if (playerBoard[row][col] != -1) {
-                playerBoard[row][col] = -1;
-            }
-        }
-
-    }
 
 
 }
